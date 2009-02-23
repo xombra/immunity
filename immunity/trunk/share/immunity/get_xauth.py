34d63d5955b4cca99e24272c24d09fe08be66c5b
mail.twerner <mail.twerner@0b2eb328-fb4c-11dd-a295-0b7b0935bf94>
2009-02-23 22:58:28 +0000

    add get_xauth.py

---
+++ immunity/trunk/share/immunity/get_xauth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os, pwd
+
+def switch_user(target_user):
+  pwd_data = pwd.getpwnam(target_user)
+  os.setgid(pwd_data[3])
+  os.setgroups([])
+  os.setuid(pwd_data[2])
+  os.putenv("USER", pwd_data[0])
+  os.unsetenv("SUDO_USER")
+  homedir = pwd_data[5]
+  os.putenv("HOME", homedir)
+  os.chdir(homedir)
+
+def get_xauth():
+  os.execl("/usr/bin/xauth", "xauth", "nlist", ":0")
+
+def main():
+  sudo_user = os.getenv("SUDO_USER")
+  switch_user(sudo_user)
+  get_xauth()
+
+main()
