250ea633642d2a272b2f103a0dd08ae7d717cd11
mail.twerner <mail.twerner@0b2eb328-fb4c-11dd-a295-0b7b0935bf94>
2009-03-17 21:28:15 +0000

    more fine grained capabilities

--- immunity/trunk/share/immunity/get_xauth.py
+++ immunity/trunk/share/immunity/get_xauth.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
-import os, pwd
+import immunity, os, pwd
 
 def switch_user(target_user):
   pwd_data = pwd.getpwnam(target_user)
+  immunity.set_cap("cap_setgid+ep cap_setuid+p")
   os.setgid(pwd_data[3])
   os.setgroups([])
+  immunity.set_cap("cap_setuid+ep")
   os.setuid(pwd_data[2])
+  immunity.set_cap("")
   os.putenv("USER", pwd_data[0])
   homedir = pwd_data[5]
   os.putenv("HOME", homedir)
@@ -16,6 +19,7 @@ def get_xauth():
   os.execl("/usr/bin/xauth", "xauth", "nlist", ":0")
 
 def main():
+  immunity.set_cap("cap_setgid,cap_setuid+p")
   sudo_user = os.getenv("SUDO_USER")
   switch_user(sudo_user)
   get_xauth()
