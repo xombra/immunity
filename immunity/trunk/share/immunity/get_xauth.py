3b820d09843ff22763ee8ccc479170c1bf06fe61
mail.twerner <mail.twerner@0b2eb328-fb4c-11dd-a295-0b7b0935bf94>
2009-03-20 21:21:37 +0000

    Remove last reference to tmp directory.

--- immunity/trunk/share/immunity/get_xauth.py
+++ immunity/trunk/share/immunity/get_xauth.py
@@ -2,8 +2,5 @@
 
 import immunity, os
 
-def main():
-  immunity.set_cap("")
-  os.execl("/usr/bin/xauth", "xauth", "nlist", ":0")
-
-main()
+immunity.set_cap("")
+os.execl("/usr/bin/xauth", "xauth", "nlist", ":0")
