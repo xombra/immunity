cf3cdb00958cccbbf538d2e7f3f209b991970259
mail.twerner <mail.twerner@0b2eb328-fb4c-11dd-a295-0b7b0935bf94>
2009-03-29 19:50:30 +0000

    increase timeout to 1200 seconds for untrusted connections

--- immunity/trunk/share/immunity/get_xauth.py
+++ immunity/trunk/share/immunity/get_xauth.py
@@ -5,7 +5,7 @@ import immunity, os, sys, tempfile
 immunity.set_cap("")
 xauthfile, name = tempfile.mkstemp()
 os.spawnl(os.P_WAIT, "/usr/bin/xauth", "xauth", "-f", name, "generate",
-    ":0", ".", "untrusted")
+    ":0", ".", "untrusted", "timeout", "1200")
 xauthfile = open(name, "r")
 sys.stdout.write(xauthfile.read())
 os.remove(name)
